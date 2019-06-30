8fec968c853487163af67f7086c2e6f2b0473d0e
rafaelsantosbraz <rafaelsantosbrazpfi@gmail.com>
2019-06-29 22:26:30 -0300

    tentativa de criação de menu lateral

--- FaltmetroGPS/app/src/main/java/com/u/faltmetrogps/MainActivity.java
+++ FaltmetroGPS/app/src/main/java/com/u/faltmetrogps/MainActivity.java
@@ -1,7 +1,9 @@
 package com.u.faltmetrogps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.AndroidViewModel;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void abrirMenuLateral(android.view.View view){
+        startActivity(new Intent(getApplicationContext(), ActivityMenuLateral.class));
     }
 }
